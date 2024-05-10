e70743e55b7bca07c7ec4386a1748c7b62bac26b
unknown <prasadnti@gmail.com>
2024-05-10 10:22:46 +0530

    imageGallery and it's popup responsive UI issues fixed

--- frontend/src/Frontend/Components/ImageGalleryComponent.jsx
+++ frontend/src/Frontend/Components/ImageGalleryComponent.jsx
@@ -52,7 +52,7 @@ const ImageGalleryComponent = ({ pageType, componentEdit }) => {
             <div className="row">
               {imageGallery.length > 0 &&
                 imageGallery?.map((item, index) => (
-                  <div className="col-4 mb-4" key={item.id}>
+                  <div className="col-sm-6 col-md-4 mb-4" key={item.id}>
                     <img
                       src={getImagePath(item.path)}
                       alt={item.alternitivetext}
