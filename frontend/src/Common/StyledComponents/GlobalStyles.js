92ed0b2a71367bf69143cd2b835d652048766eea
unknown <prasadnti@gmail.com>
2024-07-19 15:03:38 +0530

    Live testing changes

--- frontend/src/Common/StyledComponents/GlobalStyles.js
+++ frontend/src/Common/StyledComponents/GlobalStyles.js
@@ -312,10 +312,10 @@ body {
       }
       
       .newsModel img {
-        width: 100%;
-        height: 200px;
-        object-fit: cover;
-        object-position: 0%;
+        // width: 100%;
+        height: 320px;
+        // object-fit: cover;
+        // object-position: 0%;
       }
       
       .newsModalWrapper {
